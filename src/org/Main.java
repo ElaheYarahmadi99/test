3cb9a2d5097cdc8f05dda369352295c1d8f51369
ehsanSadri <programlab98@gmail.com>
2020-07-31 16:55:28 +0430

    Optimized Main Method

--- src/org/Main.java
+++ src/org/Main.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        long code = scanner.nextLong();
         Validation validation = new Validation();
-        validation.get(code);
+        validation.get(new Scanner(System.in).nextLong());
     }
 }
