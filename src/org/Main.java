a0fd361f5d7be1250297ae866b5fc3783cffa579
ehsanSadri <programlab98@gmail.com>
2020-07-31 17:18:23 +0430

    HI Message added

--- src/org/Main.java
+++ src/org/Main.java
@@ -10,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         Validation validation = new Validation();
         validation.get(new Scanner(System.in).nextLong());
+        System.out.println("Hello My Name Is Ehsan");
     }
 }
